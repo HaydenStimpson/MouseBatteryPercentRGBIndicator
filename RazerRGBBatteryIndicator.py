1a5d41d4951722b03e542c201197704ff8b4fee4
Hayden Stimpson <haydenstimpson9009@gmail.com>
2024-05-29 00:10:05 +1200

    Added comments about what functions do, changed timing so it can update battery every 15 minutes

--- RazerRGBBatteryIndicator.py
+++ RazerRGBBatteryIndicator.py
@@ -7,16 +7,17 @@ import usb.util
 from usb.backend import libusb1
 import requests
 
+# Initialize id variables - change these to use with different Razer mice
 vid = 0x1532
 pid = 0x007b
-wired_pid = 0x007a # Not sure if needed
 transaction_id = b"\xff"
 
-# Code to get battery percentage (next three functions) adapted from
+# Code to get battery percentage (get_battery_percent(), battery_msg(), get_mouse()) adapted from
 # https://github.com/hsutungyu/razer-mouse-battery-windows/blob/main/mamba.pyw
 
 def get_battery_percent():
-    # send message to mouse, wait some time, then record the returned answer
+    """"Send message to mouse, wait some time, then record the returned answer
+    Returns int from 0 to 100"""
     mouse = get_mouse()
     mouse.ctrl_transfer(bmRequestType=0x21, bRequest=0x09, wValue=0x300, data_or_wLength=battery_msg(), wIndex=0x00)
     time.sleep(0.5)
@@ -26,59 +27,62 @@ def get_battery_percent():
     return int(result[9] / 255 * 100)
 
 def battery_msg():
-    # Generate message to send to the mouse via USB - message asks for current battery percent
+    """Generate and return message to send to the mouse via USB - message asks for current battery percent"""
     msg = b"\x00" + transaction_id + b"\x00\x00\x00\x02\x07\x80" + bytes(80) + b"\x85\x00"
     return msg
 
 def get_mouse():
-    # Find wireless mouse based on its various ids
+    """Find and return wireless mouse based on its various ids"""
     backend = libusb1.get_backend()
     mouse = usb.core.find(idVendor = vid, idProduct = pid, backend = backend)
     return mouse
 
-battery = get_battery_percent()
-print(battery)
+def find_device_uri():
+    """Check the local Razer API server to find the uri of the specific device. I only have one."""
+    initialize_json = {
+        "title": "Razer Battery Indicator",
+        "description": "Razer mouse RGB battery indicator",
+        "author": {
+            "name": "Hayden Stimpson",
+            "contact": "https://haydenstimpson.github.io/"
+        },
+        "device_supported": [
+            "mouse",
+        ],
+        "category": "application"
+    }
 
-
-
-# Generate 'Instance URI'
-initialize_json = {
-    "title": "Razer Battery Indicator",
-    "description": "Razer mouse RGB battery indicator",
-    "author": {
-        "name": "Hayden Stimpson",
-        "contact": "https://haydenstimpson.github.io/"
-    },
-    "device_supported": [
-        "mouse",
-    ],
-    "category": "application"
-}
-
-returned_uri = requests.post("http://localhost:54235/razer/chromasdk", json=initialize_json)
-print(returned_uri.text)
-uri = json.loads(returned_uri.text)['uri']
-
-
+    returned_uri = requests.post("http://localhost:54235/razer/chromasdk", json=initialize_json)
+    print(returned_uri.text)
+    uri = json.loads(returned_uri.text)['uri']
+    return uri
 
 def generate_hexadecimal_color(blue, green, red):
+    """Return a hexadecimal version of RGB"""
     return '%02x%02x%02x' % (blue, green, red)
 
 def convert_battery_percent_to_color(battery_percent):
-    # Color steps from 100 to 0 % battery
+    """Generate RGB values scaling smoothly from green to red based on battery percent."""
     blue = 0
     green = int((battery_percent * 255 / 100))
     red = int(255 - green)
     return generate_hexadecimal_color(blue, green, red)
 
+time.sleep(10)
+uri = find_device_uri()
 while True:
-    color = int(convert_battery_percent_to_color(battery), 16)
-    rgb_json = {
-        "effect": "CHROMA_STATIC",
-        "param": {
-            "color":color
+    # Update battery percent every 15 minutes
+    battery = get_battery_percent()
+    print(battery)
+    # Need to update API every 10 seconds to maintain connection
+    for x in range(90):
+        color = int(convert_battery_percent_to_color(battery), 16)
+        rgb_json = {
+            "effect": "CHROMA_STATIC",
+            "param": {
+                "color": color
+            }
         }
-    }
-    color_post_return = requests.put(uri + "/mouse", json=rgb_json)
-    print(color_post_return.text)
-    time.sleep(3)
+        color_post_return = requests.put(uri + "/mouse", json=rgb_json)
+        print(color_post_return.text)
+        time.sleep(10)
